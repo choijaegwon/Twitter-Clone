16e64fdcf5d4ee330880090c3720fb7bf4e4af34
choijaegwon <chl9338@naver.com>
2022-10-19 04:49:49 +0900

    [네비게이션 컨트롤러 설정]

--- Twitter-Clone/Controller/MainTabController.swift
+++ Twitter-Clone/Controller/MainTabController.swift
@@ -25,26 +25,43 @@ class MainTabController: UITabBarController {
     func configtureViewControllers() {
         // TabBar에 추가하기
         let feed = FeedController()
-        feed.tabBarItem.image = UIImage(named: "home_unselected")
+        let nav1 = templatNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
         
         let explore = ExploreController()
-        explore.tabBarItem.image = UIImage(named: "search_unselected")
+        let nav2 = templatNavigationController(image: UIImage(named: "search_unselected"), rootViewController: explore)
         
         let notifications = NotificationsController()
-        notifications.tabBarItem.image = UIImage(named: "search_unselected")
+        let nav3 = templatNavigationController(image: UIImage(named: "search_unselected"), rootViewController: notifications)
         
         let conversations = ConversationsController()
-        conversations.tabBarItem.image = UIImage(named: "search_unselected")
+        let nav4 = templatNavigationController(image: UIImage(named: "search_unselected"), rootViewController: conversations)
         
-        viewControllers = [feed, explore, notifications, conversations]
+        viewControllers = [nav1, nav2, nav3, nav4]
+    }
+    
+    // navBar를 편하게 만들어주는 함수.
+    func templatNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        
+        nav.tabBarItem.image = image
+        nav.navigationBar.barTintColor = .white
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        nav.navigationBar.standardAppearance = appearance
+        nav.navigationBar.scrollEdgeAppearance = nav.navigationBar.standardAppearance
+        
+        return nav
     }
     
     func uiTabBarSetting() {
         if #available(iOS 15.0, *) {
             let appearance = UITabBarAppearance()
+            
             appearance.configureWithOpaqueBackground()
             appearance.backgroundColor = .white
-            
             tabBar.standardAppearance = appearance
             tabBar.scrollEdgeAppearance = appearance
         }
