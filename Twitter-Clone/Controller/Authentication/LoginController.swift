6ea4e0c8aa3b6762f26887280c90b05d4fa16c3c
choijaegwon <chl9338@naver.com>
2022-10-20 00:11:30 +0900

    [TextView 리팩토링]

--- Twitter-Clone/Controller/Authentication/LoginController.swift
+++ Twitter-Clone/Controller/Authentication/LoginController.swift
@@ -20,31 +20,27 @@ class LoginController: UIViewController {
     }()
     
     private lazy var emailContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        let iv = UIImageView()
-        view.addSubview(iv)
-        iv.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
-        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
-        iv.setDimensions(width: 24, height: 24)
-        
+        let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
         return view
     }()
     
     private lazy var passwordContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemPurple
-        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        let iv = UIImageView()
-        view.addSubview(iv)
-        iv.image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
-        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
-        iv.setDimensions(width: 24, height: 24)
-        
+        let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
         return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        // 비밀번호 숨기기
+        tf.isSecureTextEntry = true
+        return tf
     }()
     
     // MARK: - Lifecycle
@@ -71,6 +67,6 @@ class LoginController: UIViewController {
         stack.spacing = 8
         
         view.addSubview(stack)
-        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, height: 100)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingLeft: 16, paddingRight: 16)
     }
 }
---
+++ Twitter-Clone/Utils/Utilities.swift
@@ -0,0 +1,43 @@
+//
+//  Utilities.swift
+//  Twitter-Clone
+//
+//  Created by Jae kwon Choi on 2022/10/19.
+//
+
+import UIKit
+
+class Utilities {
+    
+    func inputContainerView(withImage image: UIImage, textField: UITextField) -> UIView {
+        let view = UIView()
+        let iv = UIImageView()
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        iv.image = image
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        
+        view.addSubview(textField)
+        textField.anchor(left: iv.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        // 입력창 아래 흰선 만들기
+        let dividerView = UIView()
+        dividerView.backgroundColor = .white
+        view.addSubview(dividerView)
+        dividerView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, height: 0.76)
+        
+        return view
+    }
+    
+    func textField(withPlaceholder placeholder: String) -> UITextField {
+        let tf = UITextField()
+        tf.placeholder = placeholder
+        tf.textColor = .white
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        return tf
+    }
+    
+}
