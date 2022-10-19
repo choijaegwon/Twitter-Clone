c98110e430c3a7a847efa5c45987bcde7e541449
choijaegwon <chl9338@naver.com>
2022-10-19 21:52:11 +0900

    [Button에 addTarget추가하기]

--- Twitter-Clone/Controller/MainTabController.swift
+++ Twitter-Clone/Controller/MainTabController.swift
@@ -14,8 +14,9 @@ class MainTabController: UITabBarController {
     let actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .white
-        button.backgroundColor = .blue
+        button.backgroundColor = .twitterBlue
         button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -28,6 +29,13 @@ class MainTabController: UITabBarController {
         configtureViewControllers()
         configureUI()
     }
+    
+    // MARK: - Selectors
+    
+    @objc func actionButtonTapped() {
+        print(#function)
+    }
+
     
     // MARK: - Heplers
     func configureUI() {
