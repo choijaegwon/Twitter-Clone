a54de3a3097fc6d1d69ea5bda93f66b8e3e3a545
choijaegwon <chl9338@naver.com>
2022-10-21 01:21:07 +0900

    [Firebase에 사용자 이미지 업로드하기]

--- Twitter-Clone/Controller/Authentication/RegistrationController.swift
+++ Twitter-Clone/Controller/Authentication/RegistrationController.swift
@@ -111,23 +111,40 @@ class RegistrationController: UIViewController {
         guard let fullname = fullnameTextField.text else { return }
         guard let username = usernameTextField.text else { return }
         
-        // 파이어베이스에 회원가입
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if let error = error {
-                print("DEBUG: Error is \(error.localizedDescription)")
-                return
-            }
-            
-            // 사용자의 User UID
-            guard let uid = result?.user.uid else { return }
-            // 저장할 값을 딕셔너리형태로 만들어준다.
-            let values = ["email": email, "username": username, "fullname": fullname]
-            // 저장할 경로 Database.database().reference()까진 같고,
-            // 그뒤 "users"란 키를 추가할건데 그걸 .child(uid)에 추가할 것이다.
-            let ref = Database.database().reference().child("users").child(uid)
-            // 추가하는 메서드 .updateChildValues이고, values을 넣어준다.
-            ref.updateChildValues(values) { error, ref in
-                print("DEBUG: Seuccessfully updated user information..")
+        
+        // 이미지를 jpeg로 압축하기
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        // 파일이름을 uuid로 만들어주기
+        let filename = NSUUID().uuidString
+        let storageRef = STORAGE_PROFILE_IMAGES.child(filename)
+        
+        storageRef.putData(imageData, metadata: nil) { meta, error in
+            storageRef.downloadURL { url, error in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                // 파이어베이스에 회원가입
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error is \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    // 사용자의 User UID
+                    guard let uid = result?.user.uid else { return }
+                    
+                    // 저장할 값을 딕셔너리형태로 만들어준다.
+                    let values = ["email": email,
+                                  "username": username,
+                                  "fullname": fullname,
+                                  "profileImageUrl": profileImageUrl]
+                    
+                    // REF_USERS란? 저장할 경로 Database.database().reference()까진 같고,
+                    // 그뒤 "user"란 키를 추가할건데 그걸 .child(uid)에 추가할 것이다. -> REF_USERS.child(uid)로 변경
+                    // 추가하는 메서드 .updateChildValues이고, values을 넣어준다.
+                    REF_USERS.child(uid).updateChildValues(values) { error, ref in
+                        print("DEBUG: Seuccessfully updated user information..")
+                    }
+                }
             }
         }
     }
---
+++ Twitter-Clone/Utils/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Twitter-Clone
+//
+//  Created by Jae kwon Choi on 2022/10/21.
+//
+
+import Firebase
+
+// 저장할 경로 Storage.storage().reference()
+// profile_images란 폴더를 추가해준다. 그후
+// .putData 란 메서드를 이용해서 이미지를 넘겨주고
+// downloadURL를 사용해서 profileImageUrl를 얻어온다.
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+// 저장할 경로 Database.database().reference()까진 같고,
+// 그뒤 "users"란 키를 추가할건데 그걸 .child(uid)에 추가할 것이다. -> REF_USERS.child(uid)로 변경
+// 추가하는 메서드 .updateChildValues이고, values을 넣어준다.
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("uesrs")
+
