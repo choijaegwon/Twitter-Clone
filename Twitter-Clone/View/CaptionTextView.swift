a04114949a70775adfa8f2d7011927f7ad2c46fe
choijaegwon <chl9338@naver.com>
2022-10-21 23:11:32 +0900

    [View폴더에 CaptionTextView 추가해서 사용하기]

---
+++ Twitter-Clone/View/CaptionTextView.swift
@@ -0,0 +1,49 @@
+//
+//  CaptionTextView.swift
+//  Twitter-Clone
+//
+//  Created by Jae kwon Choi on 2022/10/21.
+//
+
+import UIKit
+
+class CaptionTextView: UITextView {
+    
+    // MARK: - Properties
+    
+    let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.text = "What's happening?"
+        return label
+    }()
+    
+    // MARK: - Lifecycel
+
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        
+        backgroundColor = .white
+        font = UIFont.systemFont(ofSize: 16)
+        isScrollEnabled = false
+        heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        addSubview(placeholderLabel)
+        placeholderLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 4)
+        
+        // 텍스트가 변경될 때마다 호출하기
+        NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+
+    @objc func handleTextInputChange() {
+        // 텍스트가 비어있지 않으면 placeholderLabel 숨기기, 텍스트가 비어있으면 placeholderLabel 보여주기.
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+}
