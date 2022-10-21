598788f286cc9e44cb3d6c81eadd7a8d6a64950f
choijaegwon <chl9338@naver.com>
2022-10-21 22:21:30 +0900

    [이전뷰에서 다음뷰로 user정보 넘겨주기] PS. 좋은 코드인 것 같다.

--- Twitter-Clone/Controller/UploadTweetController.swift
+++ Twitter-Clone/Controller/UploadTweetController.swift
@@ -11,6 +11,9 @@ class UploadTweetController: UIViewController {
     
     // MARK: - Properties
     
+    // MainView에서 넘어온 user를 담을 변수
+    private let user: User
+    
     private lazy var actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = .twitterBlue
@@ -25,7 +28,26 @@ class UploadTweetController: UIViewController {
         return button
     }()
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        return iv
+    }()
+    
     // MARK: - Lifecycel
+    
+    // user정보를 담아올 변수
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,8 +70,17 @@ class UploadTweetController: UIViewController {
     // MARK: - Helpers
     
     func configureUI() {
-        uiTabBarSetting()
         view.backgroundColor = .white
+        configureNavigationBar()
+        
+        view.addSubview(profileImageView)
+        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
+                                paddingTop: 16, paddingLeft: 16)
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
+    }
+    
+    func configureNavigationBar() {
+        uiNavBarSetting()
         navigationController?.navigationBar.barTintColor = .white
         navigationController?.navigationBar.isTranslucent = false
         
@@ -57,7 +88,7 @@ class UploadTweetController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
     }
     
-    func uiTabBarSetting() {
+    func uiNavBarSetting() {
         if #available(iOS 15.0, *) {
             let appearance = UINavigationBarAppearance()
             appearance.configureWithDefaultBackground()
