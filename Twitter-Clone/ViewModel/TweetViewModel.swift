baa553dc72240116b31a515428b2078a7cc66e11
choijaegwon <chl9338@naver.com>
2022-10-22 23:39:20 +0900

    [트윗한 시간과 현재시간을 비교해서 나타내주기]

--- Twitter-Clone/ViewModel/TweetViewModel.swift
+++ Twitter-Clone/ViewModel/TweetViewModel.swift
@@ -16,10 +16,24 @@ struct TweetViewModel {
         return user.profileImageUrl
     }
     
+    var timestamp: String {
+        // 어떻게 표현할건지
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .day, .weekdayOrdinal]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        // 현재 시간
+        let now = Date()
+        // 트윗을 했을때시간과, 현재시간을 비교한다.
+        return formatter.string(from: tweet.timestamp, to: now) ?? "2m"
+    }
+    
     var userInfoText: NSAttributedString {
         let title = NSMutableAttributedString(string: user.fullname, attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
         
         title.append(NSAttributedString(string: " @\(user.username)", attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
+        
+        title.append(NSAttributedString(string: " ㆍ @\(timestamp)전", attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
         
         return title
     }
