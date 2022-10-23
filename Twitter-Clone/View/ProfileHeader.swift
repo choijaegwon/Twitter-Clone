4b61deffe7d352f15c8407bdb6c6d1dd6f652ccd
choijaegwon <chl9338@naver.com>
2022-10-24 00:33:37 +0900

    [Delegate을 활용한 사용자 프로필 닫기] PS. delegate패턴을 공부하기 좋은 코드

--- Twitter-Clone/View/ProfileHeader.swift
+++ Twitter-Clone/View/ProfileHeader.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol ProfileHeaderDelegate: AnyObject {
+    func handleDismissal()
+}
+
 class ProfileHeader: UICollectionReusableView {
     
     // MARK: - Properties
@@ -16,6 +20,8 @@ class ProfileHeader: UICollectionReusableView {
             configure()
         }
     }
+    
+    weak var delegate: ProfileHeaderDelegate?
     
     private let filterBar = ProfileFilterView()
     
@@ -158,7 +164,7 @@ class ProfileHeader: UICollectionReusableView {
     // MARK: - Selectors
     
     @objc func handleDismissal() {
-        
+        delegate?.handleDismissal()
     }
     
     @objc func handleEditProfileFollow() {
