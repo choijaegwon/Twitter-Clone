f10a46b0c329a508a8500d363ecc2f74318c123e
choijaegwon <chl9338@naver.com>
2022-10-27 00:51:19 +0900

    [NotificationViewModel 생성] + [NotificationCell을 firebase안에있는 데이터꺼내서 보여주기]

--- Twitter-Clone/View/NotificationCell.swift
+++ Twitter-Clone/View/NotificationCell.swift
@@ -11,6 +11,12 @@ class NotificationCell: UITableViewCell {
     
     // MARK: - Properties
     
+    var notification: Notification? {
+        didSet {
+            configure()
+        }
+    }
+    
     private lazy var profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
@@ -56,5 +62,16 @@ class NotificationCell: UITableViewCell {
     
     @objc func handleProfileImageTapped() {
         
+    }
+    
+    // MARK: - Helpers
+    
+    func configure() {
+        guard let notification = notification else { return }
+        
+        let viewModel = NotificationViewModel(notification: notification)
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        notificationLabel.attributedText = viewModel.notificationText
     }
 }
---
+++ Twitter-Clone/ViewModel/NotificationViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  NotificationViewModel.swift
+//  Twitter-Clone
+//
+//  Created by Jae kwon Choi on 2022/10/27.
+//
+
+import UIKit
+
+struct NotificationViewModel {
+    
+    private let notification: Notification
+    private let type: NotificationType
+    private let user: User
+    
+    var timestampString: String? {
+        // 어떻게 표현할건지
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .day, .weekdayOrdinal]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        // 현재 시간
+        let now = Date()
+        // 트윗을 했을때시간과, 현재시간을 비교한다.
+        return formatter.string(from: notification.timestamp, to: now) ?? "2m"
+    }
+    
+    var notificationMessage: String {
+        switch type {
+        case .follow:
+            return " started following you"
+        case .like:
+            return " liked your tweet"
+        case .reply:
+            return " replied to your tweet"
+        case .retweet:
+            return " retweeted you tweet"
+        case .mention:
+            return " mentioned you in a tweet"
+        }
+    }
+    
+    var notificationText: NSAttributedString? {
+        guard let timestamp = timestampString else { return nil}
+        let attributedText = NSMutableAttributedString(string: user.username, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 12)])
+        attributedText.append(NSMutableAttributedString(string: notificationMessage, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]))
+        attributedText.append(NSAttributedString(string: " \(timestamp)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12), NSAttributedString.Key.foregroundColor: UIColor.lightGray]))
+        
+        return attributedText
+    }
+    
+    var profileImageUrl: URL? {
+        return user.profileImageUrl
+    }
+    
+    init(notification: Notification) {
+        self.notification = notification
+        self.type = notification.type
+        self.user = notification.user
+    }
+}
