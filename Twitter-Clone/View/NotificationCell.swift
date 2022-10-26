199b068b5707adbf421b91e0e42f2d076609dde2
choijaegwon <chl9338@naver.com>
2022-10-27 00:00:20 +0900

    [NotificationCell 생성] + [NotificationsController에 cell 등록하기]

---
+++ Twitter-Clone/View/NotificationCell.swift
@@ -0,0 +1,60 @@
+//
+//  NotificationCell.swift
+//  Twitter-Clone
+//
+//  Created by Jae kwon Choi on 2022/10/26.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 40 / 2
+        iv.backgroundColor = .twitterBlue
+        // 이미지는 버튼처럼 클릭이 안되기 때문에, UITapGestureRecognizer를 사용해야한다.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfileImageTapped))
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        
+        return iv
+    }()
+    
+    let notificationLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Some test notification message"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let stack = UIStackView(arrangedSubviews: [profileImageView, notificationLabel])
+        stack.spacing = 8
+        stack.alignment = .center
+        
+        addSubview(stack)
+        stack.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        stack.anchor(right: rightAnchor, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selector
+    
+    @objc func handleProfileImageTapped() {
+        
+    }
+}
