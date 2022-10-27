6c4e99853f02d769011b2ccb5b0c7dd18d828bdd
choijaegwon <chl9338@naver.com>
2022-10-27 17:10:17 +0900

    [ProfileHeader의 underlineView을 ProfileFilterView로 코드리팩토링하며 이동]

--- Twitter-Clone/View/ProfileFilterView.swift
+++ Twitter-Clone/View/ProfileFilterView.swift
@@ -29,6 +29,12 @@ class ProfileFilterView: UIView {
         return cv
     }()
     
+    private let underlineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -42,6 +48,11 @@ class ProfileFilterView: UIView {
         
         addSubview(collectionView)
         collectionView.addConstraintsToFillView(self)
+    }
+    
+    override func layoutSubviews() {
+        addSubview(underlineView)
+        underlineView.anchor(left: leftAnchor, bottom: bottomAnchor, width: frame.width / 3, height: 2)
     }
     
     required init?(coder: NSCoder) {
@@ -73,6 +84,13 @@ extension ProfileFilterView: UICollectionViewDataSource {
 extension ProfileFilterView: UICollectionViewDelegate {
     // 어떤게 클릭되었는지 indexPath로 넘겨주기
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        let xPosition = cell?.frame.origin.x ?? 0
+        
+        UIView.animate(withDuration: 0.3) {
+            self.underlineView.frame.origin.x = xPosition
+        }
+        
         delegate?.filterView(self, didSelect: indexPath)
     }
 }
